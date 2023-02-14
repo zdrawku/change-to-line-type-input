2ee868f330553a5f42a850c29e57b5433ae82569
zdrawku <zkolev@infragistics.com>
2023-02-14 13:56:42 +0200

    recommended changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { IGX_INPUT_GROUP_TYPE } from '@infragistics/igniteui-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{provide: IGX_INPUT_GROUP_TYPE, useValue: 'line' }]
 })
 export class AppComponent {
   title = 'Home - IgniteUI for Angular';
